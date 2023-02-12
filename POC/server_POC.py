686d0a2b1b94ea3b047b224e9d418da52e749639
KrysCat-KitKat <35955774+KrysCat-KitKat@users.noreply.github.com>
2023-02-12 16:46:29 +0100

    Command for Linux

--- POC/server_POC.py
+++ POC/server_POC.py
@@ -4,7 +4,7 @@ def simple_app(environ, start_response):
     status = '200 OK'
     headers = [('Content-type', 'application/json'), ("Server", "C2-EX-MACHINA")]
     start_response(status, headers)
-    return (b'{"NextRequestTime":1466607345,"Tasks":[{"Type":"COMMAND","Data":"tasklist","Id":0},{"Type":"DOWNLOAD","Filename":"test.txt","Data":"my content","Id":1},{"Type":"UPLOAD","Data":"test.txt","Id":2}]}',)
+    return (b'{"NextRequestTime":1466607345,"Tasks":[{"Type":"COMMAND","Data":"ps faux && uname -a && sleep 10","Id":0},{"Type":"DOWNLOAD","Filename":"test.txt","Data":"my content","Id":1},{"Type":"UPLOAD","Data":"test.txt","Id":2}]}',)
 
 with make_server('127.0.0.1', 8000, simple_app) as httpd:
     print("Listening on port 8000 (http://127.0.0.1:8000/)....")
